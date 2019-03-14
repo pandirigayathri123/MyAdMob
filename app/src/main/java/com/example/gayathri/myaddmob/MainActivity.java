0a27dc77b0ab93bf8bc313427ed823b39564ddb7
gayathri <gayathripandiri02@gmail.com>
2019-03-14 12:46:16 +0530

    MyAddMob

---
+++ app/src/main/java/com/example/gayathri/myaddmob/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.gayathri.myaddmob;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity {
+    AdView adView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MobileAds.initialize(this);
+        adView=findViewById(R.id.add);
+        AdRequest adRequest= new  AdRequest.Builder().build();
+        adView.loadAd(adRequest);
+
+    }
+}
